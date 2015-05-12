215269f4398752ec4b0300ea0a9a15d2982dfc7e
Evren Kutar <furiston@gmail.com>
2015-05-12 15:09:49 +0300

    add login constants

--- app/app.js
+++ app/app.js
@@ -19,4 +19,18 @@ angular.module('zaerp', ['ngRoute', 'zaerp.login', 'zaerp.view2', 'zaerp.version
                 }
             }
         });
+    }).
+    constant("USER_ROLES", {
+        all: "*",
+        admin: "admin",
+        editor: "editor",
+        guest: "guest"
+    }).
+    constant('AUTH_EVENTS', {
+        loginSuccess: 'auth-login-success',
+        loginFailed: 'auth-login-failed',
+        logoutSuccess: 'auth-logout-success',
+        sessionTimeout: 'auth-session-timeout',
+        notAuthenticated: 'auth-not-authenticated',
+        notAuthorized: 'auth-not-authorized'
     });
