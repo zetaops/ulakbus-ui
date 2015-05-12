c89ac5ea2430de035d09f057cf1500f05b8ffae4
Evren Kutar <furiston@gmail.com>
2015-05-12 15:51:47 +0300

    init - load dashboard view & controller & test

--- app/app.js
+++ app/app.js
@@ -1,10 +1,10 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
-angular.module('zaerp', ['ngRoute', 'zaerp.login', 'zaerp.view2', 'zaerp.version', 'schemaForm']).
+angular.module('zaerp', ['ngRoute', 'zaerp.dashboard', 'zaerp.login', 'zaerp.version', 'schemaForm']).
     config(['$routeProvider', function ($routeProvider) {
         console.log("redirect to login");
-        $routeProvider.otherwise({redirectTo: '/view2'});
+        $routeProvider.otherwise({redirectTo: '/'});
     }]).
     run(function ($rootScope, $location) {
         $rootScope.$on("$routeChangeStart", function (event, next, current) {
