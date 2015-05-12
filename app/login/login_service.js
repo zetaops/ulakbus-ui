50d557315c88ca5441bfd161a298eccef4744d3b
Evren Kutar <furiston@gmail.com>
2015-05-12 15:11:10 +0300

    login service & session

--- app/login/login_service.js
+++ app/login/login_service.js
@@ -1,3 +1,50 @@
 /**
  * Created by evren kutar on 12/05/15.
  */
+
+"use strict";
+
+// TODO: login url cheange with correct one
+
+angular.factory('LoginService', function ($http, Session) {
+    var loginService = {};
+
+    loginService.login = function (credentials) {
+        return $http
+            .post('http://127.0.0.1:8000/login', credentials)
+            .then(function (res) {
+                Session.create(res.data.id, res.data.user.id,
+                    res.data.user.role);
+                return res.data.user;
+            });
+    };
+
+    authService.isAuthenticated = function () {
+        return !!Session.userId;
+    };
+
+    authService.isAuthorized = function (authorizedRoles) {
+        if (!angular.isArray(authorizedRoles)) {
+            authorizedRoles = [authorizedRoles];
+        }
+        return (authService.isAuthenticated() &&
+        authorizedRoles.indexOf(Session.userRole) !== -1);
+    };
+
+    return authService;
+});
+
+// TODO: initial service not working!!
+
+angular.service('Session', function () {
+    this.create = function (sessionId, userId, userRole) {
+        this.id = sessionId;
+        this.userId = userId;
+        this.userRole = userRole;
+    };
+    this.destroy = function () {
+        this.id = null;
+        this.userId = null;
+        this.userRole = null;
+    };
+});
