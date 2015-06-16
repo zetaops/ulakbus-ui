7467aa92c8aa23da6589a1bfec8e8df132937c9b
Evren Kutar <furiston@gmail.com>
2015-06-16 10:32:32 +0300

    student add functions and form generator first use

---
+++ app/student/student_add.js
@@ -0,0 +1,19 @@
+/**
+ * Copyright (C) 2015 ZetaOps Inc.
+ *
+ * This file is licensed under the GNU General Public License v3
+ * (GPLv3).  See LICENSE.txt for details.
+ */
+
+'use strict';
+
+var studentAdd = angular.module('zaerp.student.add', ['ngRoute', 'schemaForm', 'formGenerator']);
+
+
+studentAdd.controller('StudentAddCtrl', function($scope, $http, $timeout, $log, Generator, RESTURL){
+    $scope.form = Generator.generate('add_student', '');
+    $log.info($scope.form);
+    $http.get(RESTURL.url + 'add_student').then(function(res){
+        $log.info(res.data);
+    });
+});
