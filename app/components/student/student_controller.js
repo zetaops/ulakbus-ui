232baa4147c6f692d52520c66c4d66cd71da5424
Evren Kutar <furiston@gmail.com>
2015-06-17 14:41:32 +0300

    add async email validator to form service

--- app/components/student/student_controller.js
+++ app/components/student/student_controller.js
@@ -18,23 +18,17 @@ var student = angular.module('zaerp.student.add', ['ngRoute', 'schemaForm', 'for
  * to add student, provide form with form generator
  */
 
-student.controller('StudentAddCtrl', function($scope, $http, $log, $q, $timeout, Generator, $routeParams){
+student.controller('StudentAddCtrl', function($scope, $http, $log, Generator, $routeParams){
     Generator.get_form('add_student', $routeParams).then(function(d){
         $scope.schema = d.schema;
         $scope.form = d.form;
-        $scope.form[0].$asyncValidators = {
-            emailNotValid: function (value) {
-                var deferred = $q.defer();
-                $timeout(function () {
-                    if (Generator.isValidEmail(value)) {
-                        deferred.resolve();
-                    } else {
-                        deferred.reject();
-                    }
-                }, 500);
-                return deferred.promise;
+        $scope.form[0].$asyncValidators = Generator.asyncValidators;
+        $scope.form.push(
+            {
+                type: "submit",
+                title: "Save"
             }
-        }
+        );
     });
     $scope.onSubmit = function (form) {
         $scope.$broadcast('schemaFormValidate');
--- app/zetalib/forms/form_service.js
+++ app/zetalib/forms/form_service.js
@@ -7,7 +7,7 @@
 
 var form_generator = angular.module('formService', []);
 
-form_generator.factory('Generator', function ($http, RESTURL) {
+form_generator.factory('Generator', function ($http, $q, $timeout, RESTURL) {
     var generator = {};
     generator.generate = function (modelObject) {
         return generator.group(modelObject);
@@ -32,6 +32,19 @@ form_generator.factory('Generator', function ($http, RESTURL) {
     generator.isValidEmail = function(email){
         var re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
         return re.test(email);
+    };
+    generator.asyncValidators = {
+        emailNotValid: function (value) {
+            var deferred = $q.defer();
+            $timeout(function () {
+                if (generator.isValidEmail(value)) {
+                    deferred.resolve();
+                } else {
+                    deferred.reject();
+                }
+            }, 500);
+            return deferred.promise;
+        }
     };
     return generator;
 });
