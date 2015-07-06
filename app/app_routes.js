69e56e4f6800d2418f2dc2a6b62070b27b61e859
Evren Kutar <furiston@gmail.com>
2015-07-06 09:32:04 +0300

    interceptors in seperate file

--- app/app_routes.js
+++ app/app_routes.js
@@ -174,27 +174,4 @@ app.config(['$routeProvider', function ($routeProvider) {
             }
         }
     });
-}).config(['$httpProvider', function ($httpProvider) {
-    /**
-     * the interceptor for all requests to check response
-     * 4xx - 5xx errors will be handled here
-     */
-    $httpProvider.interceptors.push(function ($q) {
-        return {
-            'response': function (response) {
-                //Will only be called for HTTP up to 300
-                return response;
-            },
-            'responseError': function (rejection) {
-                // if unauthorized then redirect to login page
-                if(rejection.status === 400) {
-                    location.reload();
-                }
-                if(rejection.status === 401) {
-                    location.path('#/login');
-                }
-                return $q.reject(rejection);
-            }
-        };
-    });
-}]);
+});
