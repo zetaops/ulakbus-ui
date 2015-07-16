fc249510d3661cb1d9f90f156ac02f86edea3412
Evren Kutar <furiston@gmail.com>
2015-07-16 09:43:55 +0300

    grunt file create first instance

---
+++ Gruntfile.js
@@ -0,0 +1,102 @@
+module.exports = function (grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        uglify: {
+            dist: {
+                options: {
+                    banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+                },
+                //build: {
+                //    src: 'src/<%= pkg.name %>.js',
+                //    dest: 'build/<%= pkg.name %>.min.js'
+                //},
+                files: {
+                    'dist/app.js': ['dist/app.js']
+                }
+            }
+        },
+        bower: {
+            install: {
+                options: {
+                    install: true,
+                    copy: false,
+                    targetDir: './libs',
+                    cleanTargetDir: true
+                }
+            }
+        },
+        jshint: {
+            all: ['Gruntfile.js', 'app/*.js', 'app/**/*.js']
+        },
+        karma: {
+            options: {
+                configFile: 'karma.conf.js'
+            },
+            unit: {
+                singleRun: true
+            },
+
+            continuous: {
+                singleRun: false,
+                autoWatch: true
+            }
+        },
+        html2js: {
+            dist: {
+                src: ['index.html', 'app/components/**/*.html'],
+                dest: 'tmp/templates.js'
+            }
+        },
+        concat: {
+            options: {
+                separator: ';'
+            },
+            dist: {
+                src: ['app/**/*controller.js', 'app/**/*service.js', 'app/zetalib/**/*.js', 'app.js', 'app.routes.js'],
+                dest: 'dist/app.js'
+            },
+            components: {
+                src: ['app/bower_components/**/*.min.js'],
+                dest: 'dist/bower_components/components.js'
+            }
+        },
+        watch: {
+            dev: {
+                files: ['Gruntfile.js', 'app/**/*.js', 'index.html', 'app/components/**/*.html'],
+                tasks: ['jshint', 'karma:unit', 'html2js:dist', 'concat:dist', 'clean:temp'],
+                options: {
+                    atBegin: true
+                }
+            },
+            min: {
+                files: ['Gruntfile.js', 'app/**/*.js', 'index.html', 'app/components/**/*.html'],
+                tasks: ['jshint', 'karma:unit', 'html2js:dist', 'concat:dist', 'clean:temp', 'uglify:dist'],
+                options: {
+                    atBegin: true
+                }
+            }
+        }
+    });
+
+    // Default task(s).
+    //grunt.registerTask('default', ['uglify']);
+
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.loadNpmTasks('grunt-contrib-compress');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-html2js');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-bower-task');
+    grunt.loadNpmTasks('grunt-karma');
+
+    grunt.registerTask('dev', ['bower', 'connect:server', 'watch:dev']);
+    grunt.registerTask('test', ['bower', 'jshint', 'karma:continuous']);
+    grunt.registerTask('minified', ['bower', 'connect:server', 'watch:min']);
+    grunt.registerTask('default', ['bower', 'html2js:dist', 'concat:dist',  'concat:components', 'uglify:dist']);
+
+};
--- app/main.js
+++ app/main.js
@@ -8,29 +8,28 @@
 'use strict';
 
 $script([
-    'bower_components/angular/angular.js',
-    'bower_components/angular-sanitize/angular-sanitize.min.js',
-    'bower_components/angular-animate/angular-animate.min.js',
-    'bower_components/moment/moment.js',
-    'bower_components/tv4/tv4.js',
-    'bower_components/objectpath/lib/ObjectPath.js',
-    'bower_components/angular-schema-form/dist/schema-form.min.js',
-    'bower_components/angular-schema-form/dist/bootstrap-decorator.min.js',
-
-    'app.js',
-    'zlib/general.js',
-    'forms/form_generator.js',
-    'test_view_for_generator/testfile.js',
-
-
-    'login/login.js',
-    'login/login_service.js',
-
-    'dashboard/dashboard.js',
-    'components/version/version.js',
-    'components/version/version-directive.js',
-    'components/version/interpolate-filter.js',
-    'bower_components/quantumui/dist/js/quantumui-nojq.js',
+    "bower_components/angular/angular.js",
+    "bower_components/oclazyload/dist/ocLazyLoad.js",
+    "bower_components/angular-route/angular-route.js",
+    "bower_components/angular-cookies/angular-cookies.js",
+    "bower_components/angular-resource/angular-resource.js",
+    "bower_components/angular-sanitize/angular-sanitize.js",
+    "bower_components/tv4/tv4.js",
+    "bower_components/objectpath/lib/ObjectPath.js",
+    "bower_components/angular-schema-form/dist/schema-form.js",
+    "bower_components/angular-schema-form/dist/bootstrap-decorator.js",
+    "bower_components/angular-schema-form-datepicker/bootstrap-datepicker.js",
+    "app.js",
+    "app_routes.js",
+    "zetalib/interceptors.js",
+    "zetalib/general.js",
+    "zetalib/forms/form_service.js",
+    "components/auth/auth_controller.js",
+    "components/auth/auth_service.js",
+    "components/dashboard/dashboard.js",
+    "components/staff/staff_controller.js",
+    "components/student/student_controller.js",
+    "components/dashboard/dashboard.js"
 ], function() {
     // when all is done, execute bootstrap angular application
     angular.bootstrap(document, ['zaerp']);
