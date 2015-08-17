7eede52ee086fe016e1600f209983f2df9a9eaf0
Evren Kutar <furiston@gmail.com>
2015-08-17 13:58:19 +0300

    make listnodemodal generic

--- app/app.js
+++ app/app.js
@@ -27,7 +27,7 @@ var app = angular.module(
  * Based on the environment it changes from dev to prod
  */
     constant("RESTURL", (function () {
-        return {url: "http://"+ window.location.hostname + ":9001/"};
+        return {url: "http://" + window.location.hostname + ":9001/"};
     })()).
 /**
  * USER_ROLES and AUTH_EVENTS are constant for auth functions
@@ -77,11 +77,11 @@ var app = angular.module(
 /**
  * logout directive
  */
-    directive('logout', function($http, $location){
+    directive('logout', function ($http, $location) {
         return {
-            link: function($scope, $element, $rootScope){
-                $element.on('click', function(){
-                    $http.post('http://'+window.location.hostname+':9001/logout', {}).then(function () {
+            link: function ($scope, $element, $rootScope) {
+                $element.on('click', function () {
+                    $http.post('http://' + window.location.hostname + ':9001/logout', {}).then(function () {
                         $rootScope.loggedInUser = false;
                         console.log($rootScope.loggedInUser);
                         $location.path("/login");
@@ -91,6 +91,27 @@ var app = angular.module(
             }
         }
     });
+
+/**
+ * listnode add directive
+ */
+
+    //directive('addlistnode', function () {
+    //    return {
+    //        link: function ($scope, $modal, $element) {
+    //            debugger;
+                //$element.on('click', function () {
+                //    var nodename = $element[0].firstElementChild.innerHTML;
+                //    var newitem = angular.copy($scope.listnodeform[nodename+'_1']);
+                //    console.log($scope.form);
+                //    $scope.form.splice(7, 0, newitem);
+                //    console.log($scope.form);
+                //    $scope.$broadcast('schemaFormRedraw');
+                //    $scope.$apply();
+                //});
+    //        }
+    //    }
+    //});
 
 // test the code with strict di mode to see if it works when minified
 //angular.bootstrap(document, ['ulakbus'], {
--- app/zetalib/forms/form_service.js
+++ app/zetalib/forms/form_service.js
@@ -5,9 +5,9 @@
  * (GPLv3).  See LICENSE.txt for details.
  */
 
-var form_generator = angular.module('formService', ['general']);
+var form_generator = angular.module('formService', ['general', 'ui.bootstrap']);
 
-form_generator.factory('Generator', function ($http, $q, $log, $timeout, RESTURL, FormDiff) {
+form_generator.factory('Generator', function ($http, $q, $log, $modal, $timeout, RESTURL, FormDiff) {
     var generator = {};
     generator.makeUrl = function (url) {
         return RESTURL.url + url;
@@ -16,14 +16,82 @@ form_generator.factory('Generator', function ($http, $q, $log, $timeout, RESTURL
         for (var key in forms)
             scope[key] = forms[key];
         scope.initialModel = angular.copy(scope.model);
+        //debugger;
         scope.form.push(
             {
                 type: "submit",
                 title: "Save"
             }
         );
-        scope.isCollapsed = false;
+        // if fieldset in form, make it collapsable with template
+        scope.listnodeform = {};
+        if (scope.listnodes[0] || scope.nodes[0]) {
+            angular.forEach(scope.form, function (key, val) {
+                if (typeof key == "object" && key.type == "fieldset") {
+                    // change type to use shared template for form
+                    key.type = "template";
+                    key.templateUrl = "shared/templates/fieldset.html";
+                    // check if fieldset in listnodes
+                    if (scope.listnodes.indexOf(key.title) >= 0) {
+                        scope.listnodeform[key.title] = {};
+                        scope.listnodeform[key.title]["schema"] = {
+                            "title": angular.copy(key.title),
+                            "type": "object",
+                            "properties": {},
+                            "required": []
+                        };
+                        angular.forEach(scope.schema.properties, function(k, v){
+                            angular.forEach(key.items, function(item){
+                                if(item.key == v){
+                                    scope.listnodeform[key.title]["schema"]["properties"][v] = angular.copy(k);
+                                }
+                            });
+                        });
+                        key.setType = "ListNode";
+                        scope.listnodeform[key.title]["form"] = [angular.copy(key)];
+                        scope.listnodeform[key.title]["model"] = {};
+                        key.type = "list";
+                        debugger;
+                        delete key.templateUrl;
+                        delete key.items;
+                    }
+                }
+            });
+        }
+        scope.isCollapsed = true;
         scope.object_id = scope.form_params['object_id'];
+        // open modal with given items and controller
+        scope.openmodal = function(listnode, nodeID){
+            var modalInstance = $modal.open({
+                animation: false,
+                templateUrl: 'shared/templates/listnodeModalContent.html',
+                controller: 'ListNodeModalCtrl',
+                size: 'lg',
+                resolve: {
+                    items: function () {
+                        if (nodeID){
+                            scope.listnodeform[listnode].model = 1;
+                        }
+                        return scope.listnodeform[listnode];
+                    }
+                }
+            });
+
+            modalInstance.result.then(function (childmodel, key) {
+
+                angular.forEach(childmodel, function(v, k){
+                    debugger;
+                    if (scope.model[k]){
+                        scope.model[k][v.idx] = v;
+                    } else {
+                        scope.model[k] = {};
+                        scope.model[k][v.idx] = v;
+                    }
+                    scope.$broadcast('schemaFormRedraw');
+                });
+                debugger;
+            });
+        };
         return generator.group(scope);
     };
     generator.group = function (formObject) {
@@ -84,11 +152,55 @@ form_generator.factory('Generator', function ($http, $q, $log, $timeout, RESTURL
         }
         return $http
             .post(generator.makeUrl($scope.url), data)
-            //.then(function (res) {
-            //    // todo: for now fake rest api returns 'ok' no data to
-            //    // manipulate on ui. therefor used just a log
-            //    $log.info(res);
-            //});
+        //.then(function (res) {
+        //    // todo: for now fake rest api returns 'ok' no data to
+        //    // manipulate on ui. therefor used just a log
+        //    $log.info(res);
+        //});
     };
     return generator;
 });
+
+/**
+ * ListNodeModalCtrl
+ * controller to use with openmodal function of Generator
+ * @params: $scope, $modalInstance, items
+ * @returns: return value used in openmodal's result function
+ */
+
+form_generator.controller('ListNodeModalCtrl', function ($scope, $modalInstance, items) {
+    for (var key in items)
+        $scope[key] = items[key];
+    $scope.onSubmit = function(form){
+        // send form to modalinstance result function
+        $modalInstance.close($scope.model, $scope.form.title);
+    };
+    $scope.cancel = function () {
+        $modalInstance.dismiss('cancel');
+    };
+});
+
+// todo: generic modal directive for all forms
+//form_generator.directive('openmodal', ['Generator', function (Generator, $modal) {
+//    return {
+//        link: function ($modal) {
+//            var modalInstance = $modal.open({
+//                animation: false,
+//                templateUrl: 'shared/templates/modalContent.html',
+//                controller: 'ModalInstanceCtrl',
+//                size: 'lg',
+//                resolve: {
+//                    items: function () {
+//                        return "";
+//                    }
+//                }
+//            });
+//
+//            modalInstance.result.then(function (selectedItem) {
+//                $scope.selected = selectedItem;
+//            }, function () {
+//                $log.info('Modal dismissed at: ' + new Date());
+//            });
+//        }
+//    }
+//}]);
