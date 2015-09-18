8e25baef55ec7e2da48dc5f59678e977dc8b7279
Evren Kutar <furiston@gmail.com>
2015-09-18 09:22:23 +0300

    v0.0.3.1 backendurl fix

--- app/main.js
+++ app/main.js
@@ -37,7 +37,7 @@ var app = angular.module(
  */
     constant("RESTURL", (function () {
         // todo: below backendurl definition is for development purpose and will be deleted
-        var backendurl = "http://api.ulakbus.net";
+        var backendurl = "http://api.ulakbus.net/";
         if (document.cookie.indexOf("backendurl") > -1){
             var cookiearray = document.cookie.split(';');
             angular.forEach(cookiearray, function(item){
