04068e3bee48b1790b7af718418514f4846900da
Vladimir Baranov <baranoff.vladimir@gmail.com>
2016-10-06 15:13:15 +0300

    Add basic tasks-widget backend integration

--- app/components/dashboard/dashboard_widgets_directives.js
+++ app/components/dashboard/dashboard_widgets_directives.js
@@ -14,19 +14,110 @@
  * @description Directive for .
  */
 angular.module('ulakbus.dashboard')
-    .directive('userTasks', function (WSOps) {
+    .service('TasksService', function (Utils) {
+
+        this.get_tasks = function (options) {
+            options = $.extend({
+                state: 'active',
+                inverted: false,
+                query: '',
+                wf_type: '',
+                start_date: '',
+                finish_date: ''
+            }, options);
+            var outgoing = {
+                view: '_zops_get_tasks',
+                state: options.state,            // string,   # "active", "future", "finished", "expired"
+                inverted: options.inverted,      // boolean,  # search on other people's tasks
+                query: options.query,            // string,   # optional. for searching on user's tasks
+                wf_type: options.wf_type,        // string,   # optional. only show tasks of selected wf_type
+                start_date: options.start_date,  // datetime, # optional. only show tasks starts after this date
+                finish_date: options.finish_date // datetime, # optional. only show tasks should end before this date
+            };
+            return Utils.wsRequest(outgoing).then(function (data) {
+                return data.task_list;
+            });
+        };
+
+        this.get_task_types = function () {
+            var outgoing = {
+                'view': '_zops_get_task_types'
+            };
+            return Utils.wsRequest(outgoing).then(function (data) {
+                return data.task_types;
+            });
+        };
+
+        this.get_task_actions = function (key) {
+            var outgoing = {
+                view: '_zops_get_task_actions',
+                key: key
+            };
+
+            return Utils.wsRequest(outgoing).then(function (data) {
+                return data.actions
+            });
+        };
+
+        this.get_task_detail = function (key) {
+            var outgoing = {
+                view: '_zops_get_task_detail',
+                key: key
+            };
+
+            return Utils.wsRequest(outgoing);
+        }
+
+    })
+    .directive('userTasks', function (TasksService) {
         return {
             templateUrl: 'components/dashboard/directives/user-tasks.html',
             restrict: 'E',
             replace: true,
             scope: {},
+            link: function(iScope, iElem, iAttrs){
+                TasksService.get_task_types().then(function(types){
+                    var defaultType = {
+                        name: '',
+                        title: 'Bütün Türler'
+                    };
+                    types.unshift(defaultType);
+                    iScope.taskTypes = types;
+                    iScope.taskType = '';
+                });
+            },
             controller: function ($scope) {
+
                 // todo: below are for test, will be removed
                 $scope.task_list = [
-                    {WFToken: "yfuialhfuial", title: "sample 1 workflow", description: "sample 1", wf_type: "type x", date: '02.05.2016'},
-                    {WFToken: "yfuialhfuial", title: "sample 2 workflow", description: "sample 2", wf_type: "type x", date: '02.05.2016'},
-                    {WFToken: "yfuialhfuial", title: "sample 3 workflow", description: "sample 3", wf_type: "type y", date: '02.05.2016'},
-                    {WFToken: "yfuialhfuial", title: "sample 4 workflow", description: "sample 4", wf_type: "type y", date: '02.05.2016'}
+                    {
+                        WFToken: "yfuialhfuial",
+                        title: "sample 1 workflow",
+                        description: "sample 1",
+                        wf_type: "type x",
+                        date: '02.05.2016'
+                    },
+                    {
+                        WFToken: "yfuialhfuial",
+                        title: "sample 2 workflow",
+                        description: "sample 2",
+                        wf_type: "type x",
+                        date: '02.05.2016'
+                    },
+                    {
+                        WFToken: "yfuialhfuial",
+                        title: "sample 3 workflow",
+                        description: "sample 3",
+                        wf_type: "type y",
+                        date: '02.05.2016'
+                    },
+                    {
+                        WFToken: "yfuialhfuial",
+                        title: "sample 4 workflow",
+                        description: "sample 4",
+                        wf_type: "type y",
+                        date: '02.05.2016'
+                    }
                 ];
                 /**
                  * tasks need to be regrouped by wf_type
@@ -63,7 +154,8 @@ angular.module('ulakbus.dashboard')
             replace: true,
             scope: {},
             controller: function ($scope) {
-                
+
             }
         };
-    });;
+    });
+;
