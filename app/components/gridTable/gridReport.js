d4e0c493cf2f37a54fba10c430b267340590c4dc
sparth <sparth786@gmail.com>
2017-05-05 18:19:58 +0530

    ADD, module seperated for data grid

---
+++ app/components/gridTable/gridReport.js
@@ -0,0 +1,10 @@
+/**
+ * @license Ulakbus-UI
+ * Copyright (C) 2015 ZetaOps Inc.
+ *
+ * This file is licensed under the GNU General Public License v3
+ * (GPLv3).  See LICENSE.txt for details.
+ */
+
+angular.module('ulakbus.grid', ['ui.grid', 'ui.grid.infiniteScroll']);
+
