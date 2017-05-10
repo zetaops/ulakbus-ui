893ea8c0de1c2f5a8ed9accdf5572d1831f3b3ec
sparth <sparth786@gmail.com>
2017-05-10 18:10:27 +0530

    FIX, wf: crud on selecting selectbox rref #5676

--- app/zetalib/form-service.js
+++ app/zetalib/form-service.js
@@ -907,6 +907,7 @@ angular.module('ulakbus.formService', ['ui.bootstrap'])
                             getDropdownTitleMap: function () {
                                 delete modelScope.form_params.query;
                                 formitem.gettingTitleMap = true;
+                                modelScope.form_params.wf = 'crud';
                                 generateTitleMap(modelScope)
                                     .then(function (data) {
                                         formitem.titleMap = data;
