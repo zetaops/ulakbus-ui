5ee406c1509847c76ab27dddf2e048c147209b5b
sparth <sparth786@gmail.com>
2017-08-16 14:55:38 +0530

    ADD, directives for file upload and alert in bap main page rref #5702

--- app/bap/directive/bap-directive.js
+++ app/bap/directive/bap-directive.js
@@ -66,4 +66,64 @@ angular.module('ulakbusBap')
 
             }
         };
+    })
+    /**
+     * @memberof ulakbusBap
+     * @ngdoc directive
+     * @name fileread
+     * @description Fileread directive is responsible for reading uploaded file and replace it to related model item.
+     */
+    .directive("fileread", function ($timeout) {
+        return {
+            scope: {
+                fileread: "="
+            },
+            link: function (scope, element, attributes) {
+                element.bind("change", function (changeEvent) {
+                    var reader = new FileReader();
+                    reader.onload = function (loadEvent) {
+                        scope.$apply(function () {
+                            scope.fileread = loadEvent.target.result;
+                        });
+                        $timeout(function () {
+                            //if not an image
+                            var isImage = undefined;
+                            if(changeEvent.target.files[0].type.indexOf('image') === -1){
+                                isImage = false;
+                            }else{
+                                isImage = true;
+                            }
+                            scope.$parent.model[changeEvent.target.name] = {
+                                file_name: changeEvent.target.files[0].name,
+                                file_content: scope.$parent.model[changeEvent.target.name],
+                                isImage :isImage
+                            };
+                            document.querySelector('#image-preview').src = URL.createObjectURL(changeEvent.target.files[0]);
+                        });
+                    };
+                    reader.readAsDataURL(changeEvent.target.files[0]);
+                });
+            }
+        }
+    })
+    /**
+     * @memberof ulakbusBap
+     * @ngdoc directive
+     * @name alertBox
+     * @description Triggers when `alertBox` broadcasted with alert data..
+     */
+    .directive('alertBox', function ($timeout) {
+        return {
+            templateUrl: '/shared/templates/directives/alert.html',
+            restrict: 'E',
+            replace: true,
+            link: function ($scope) {
+                $scope.$on('alertBox', function ($event, data) {
+                    $timeout(function () {
+                        delete $scope.alerts;
+                    }, 5000);
+                    $scope.alerts = [data];
+                });
+            }
+        };
     });
