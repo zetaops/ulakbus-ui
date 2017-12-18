b244b8a0d1774968ff462d45f5052ded01e461fb
zetaops <cihantoklucu1@gmail.com>
2017-12-18 12:44:19 +0300

    typo fixed

--- app/main.js
+++ app/main.js
@@ -65,7 +65,7 @@ angular.module(
     // todo: convert it to service
     .constant("RESTURL", (function () {
         // todo: below backendurl definition is for development purpose and will be deleted
-        var backendurl = location.href.indexOf('nightly') > -1 ? "//nightly.api.ulakbus.net/" : "//ap.ulakbus.net/";
+        var backendurl = location.href.indexOf('nightly') > -1 ? "//nightly.api.ulakbus.net/" : "//api.ulakbus.net/";
         if (document.cookie.indexOf("backendurl") > -1) {
             var cookiearray = document.cookie.split(';');
             angular.forEach(cookiearray, function (item) {
